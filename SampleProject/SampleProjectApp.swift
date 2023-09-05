aa96284a7e00eb005d1039e00128353537bfe007
Evan Edwin Lopez <lopez.evanedwin@gmail.com>
2023-09-05 15:32:05 +0800

    Initial Commit

---
+++ SampleProject/SampleProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleProjectApp.swift
+//  SampleProject
+//
+//  Created by d&a-m-pro  on 5/9/23.
+//
+
+import SwiftUI
+
+@main
+struct SampleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
